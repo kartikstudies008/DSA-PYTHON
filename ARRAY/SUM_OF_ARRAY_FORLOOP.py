c028db7fc20f491d783aa126513cc0e07ddd6176
Kartik <kartikstudies008@gmail.com>
2026-03-08 09:48:34 +0530

    DAY 2

---
+++ ARRAY/SUM_OF_ARRAY_FORLOOP.py
@@ -0,0 +1,18 @@
+# SUM OF ARRAY !
+arr = [2,4,6,8]
+total = 0
+
+for num in arr :
+    total = total + num 
+print(total)
+
+# PRACTICE PROBLEM
+arr2 = [1,3,5,7,9]
+total = 0
+
+for num in arr2:
+    total = total + num
+
+print(total)
+
+
---
+++ ARRAY/SUM_OF_ARRAY_WHILELOOP.py
@@ -0,0 +1,22 @@
+#SUM OF AN ARRAY WHILE LOOP !
+arr = [2,4,6,8]
+total = 0
+i = 0 
+
+while i<len(arr):
+    total = total+arr[i]
+    i+=1
+print(total)
+
+#PRACTICE PROBLEM
+arr2 = [1,3,5,7,9]
+total = 0
+i = 0
+
+while i < len(arr2):
+    total = total + arr2[i]
+    i+=1
+
+print(total)
+
+
