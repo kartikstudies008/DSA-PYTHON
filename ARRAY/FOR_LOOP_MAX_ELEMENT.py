82d6198ab57471c3c8f83cc5611d3cb819c2fea9
Kartik <kartikstudies008@gmail.com>
2026-03-10 23:23:21 +0530

    DAY 3

---
+++ ARRAY/FOR_LOOP_MAX_ELEMENT.py
@@ -0,0 +1,18 @@
+# FIND MAX NO. IN ARRAY 
+arr = [4,8,1,6]
+
+maximum = arr[0]
+
+for num in arr:
+    if  num > maximum:
+        maximum = num 
+print(maximum)
+
+# QUES 2 
+arr2 = [3, 5, 9, 2]
+maximum1 = arr2[0]
+
+for num in arr2:
+    if num > maximum1:
+        maximum1 = num 
+print(maximum1)
---
+++ ARRAY/LINEAR_SEARCH.py
@@ -0,0 +1,14 @@
+#LINEAR SEARCH
+arr = [4,8,1,6]
+target = 1
+found = False
+
+for num in arr:
+    if num == target:
+        found = True
+if found:
+    print("found")    
+else:
+    print("not found")
+        
+    
