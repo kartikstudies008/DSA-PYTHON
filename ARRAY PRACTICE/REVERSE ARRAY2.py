63d52530085f753c07a128387553aa8a98db9e20
KARTIK SHARMA <kartikstudies008@gmail.com>
2026-07-30 23:41:49 +0530

    Update REVERSE ARRAY2.py

--- ARRAY PRACTICE/REVERSE ARRAY2.py
+++ ARRAY PRACTICE/REVERSE ARRAY2.py
@@ -1,4 +1,4 @@
-arr = [12,13,14,15,16] #arr --1221118ddTGM6JU55GNE854rtstjgttn5hytjkligyih
+arr = [12,13,14,15,16] #arr --1221118ddTGM6JU55GNE854rtstjgttn5hytjkligyihi
 ukry
 left = 0
 right = len(arr)-1l
